f20b55ddc95de96c0bc9452164b5a0d7cbc8e838
tommywa-a <tomiwa95@gmail.com>
2023-02-15 11:51:34 +0100

    changed bracket type for port

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,4 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
-app.listen(port, () => console.log(`Server started on port $(port)`))
+app.listen(port, () => console.log(`Server started on port ${port}`))
