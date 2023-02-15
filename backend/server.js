a9f1395fbd33f22e11007d93881dd8da8301ee25
tommywa-a <tomiwa95@gmail.com>
2023-02-15 11:57:39 +0100

    First route

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,8 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
+app.get('/api/goals', (req, res) => {
+  res.status(200).json({message: 'Get goals'})
+})
+
 app.listen(port, () => console.log(`Server started on port ${port}`))
