7c385d65fd44b78f5f0b0e30781f96b7dfa17297
tommywa-a <80416515+tommywa-a@users.noreply.github.com>
2023-02-18 22:32:53 +0100

    Update errorMiddleware.js
    Changed res.statusCode(statusCode) to res.status(statusCode)

--- backend/errorMiddleware.js
+++ backend/errorMiddleware.js
@@ -1,7 +1,7 @@
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500
 
-  res.statusCode(statusCode)
+  res.status(statusCode)
 
   res.json({
     message: err.message,
