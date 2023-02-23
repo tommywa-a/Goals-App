8e853a9a2be212c9cd03d80f11fe2bfddd890b63
tommywa-a <tomiwa95@gmail.com>
2023-02-23 12:59:03 +0100

    Create/Set Goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -7,37 +7,41 @@ const Goal = require('../models/goalModel')
 const getGoals = asyncHandler(async (req, res) => {
 	const goals = await Goal.find()
 
-	res.status(200).json({goals});
-});
+	res.status(200).json({ goals })
+})
 
 // @desc    Set goal
 // @route POST /api/goal
 // @access Private
 const setGoal = asyncHandler(async (req, res) => {
-    if (!req.body.text) {
-        res.status(400)
-        throw new Error('Please add a text field')
-        }
-	res.status(200).json({ message: 'Set goal' });
-});
+	if (!req.body.text) {
+		res.status(400)
+		throw new Error('Please add a text field')
+	}
+	const goal = await Goal.create({
+		text: req.body.text,
+	})
+
+	res.status(200).json({ goal })
+})
 
 // @desc    Update goal
 // @route PUT /api/goals/:id
 // @access Private
 const updateGoal = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: `Updated goal ${req.params.id}` });
-});
+	res.status(200).json({ message: `Updated goal ${req.params.id}` })
+})
 
 // @desc    Delete goal
 // @route DELETE /api/goal
 // @access Private
 const deleteGoal = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: `Deleted goal ${req.params.id}` });
-});
+	res.status(200).json({ message: `Deleted goal ${req.params.id}` })
+})
 
 module.exports = {
 	getGoals,
 	setGoal,
 	updateGoal,
 	deleteGoal,
- };
+}
