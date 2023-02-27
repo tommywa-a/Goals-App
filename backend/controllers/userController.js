93f0c3ae9fc222d7460000050cb9811c5d6dde02
tommywa-a <tomiwa95@gmail.com>
2023-02-27 14:02:32 +0100

    Create User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,11 +21,27 @@ const registerUser = asyncHandler(async (req, res) => {
 		throw new Error('User already exists')
 	}
 
-  // Hash password
-  const salt = await bcrypt.genSalt(10)
-  const hashedPassword = await bcrypt.hash(password, salt)
+	// Hash password
+	const salt = await bcrypt.genSalt(10)
+	const hashedPassword = await bcrypt.hash(password, salt)
 
-	res.json({ message: 'Register User' })
+	// create user
+	const user = await User.create({
+		name,
+		email,
+		password: hashedPassword,
+	})
+
+	if (user) {
+		res.status(201).json({
+			_id: user.id,
+			name: user.name,
+			email: user.email,
+		})
+	} else {
+		res.status(400)
+		throw new Error('Invalid User Data')
+	}
 })
 
 // @desc  Authenticate a user
