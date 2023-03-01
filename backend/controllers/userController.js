321cfbeb4c429ced389918b7dc27a858bb427c74
tommywa-a <tomiwa95@gmail.com>
2023-03-01 13:32:48 +0100

    Authenticate User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,12 +48,21 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
-	const {email, password} = req.body
-	
-	// check for user email
-	const user = await User.findOne({email})
+	const { email, password } = req.body
 
-	res.json({ message: 'Register User' })
+	// check for user email
+	const user = await User.findOne({ email })
+
+	if (user && (await bcrypt.compare(password, user.password))) {
+		res.json({
+			_id: user.id,
+			name: user.name,
+			email: user.email,
+		})
+	} else {
+		res.status(400)
+		throw new Error('Invalid credentials')
+	}
 })
 
 // @desc  Get user data
