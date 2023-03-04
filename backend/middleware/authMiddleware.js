554dd648de4a3654e16faa2a3ef43c9a97ddd7c7
tommywa-a <tomiwa95@gmail.com>
2023-03-04 23:29:24 +0100

    Getting token from header and verifying token

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,7 +3,19 @@ const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 
 const protect = asyncHandler(async (req, res, next) => {
-  
+  let token
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(' ')[1]
+
+      // Verify the token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (error) {
+      
+    }
+  }
 })
 
 modules.export = { protect }
