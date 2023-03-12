07131a39a4e3102c555f5359c0dc105c53b15269
tommywa-a <tomiwa95@gmail.com>
2023-03-12 17:54:17 +0100

    Updated Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
-import {useSelector, useDispatch} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import {toast} from 'react-toastify'
+import { toast } from 'react-toastify'
 import { FaUser } from 'react-icons/fa'
 import { register, reset } from '../features/auth/authSlice'
 import Spinner from '../components/Spinner'
@@ -16,23 +16,24 @@ function Register() {
 
 	const { name, email, password, password2 } = formData
 
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
+	const navigate = useNavigate()
+	const dispatch = useDispatch()
 
-  const {user, isLoading, isError, isSuccess, message} = useSelector((state) => state.auth)
+	const { user, isLoading, isError, isSuccess, message } = useSelector(
+		(state) => state.auth
+	)
 
-  useEffect(() => {
-    if (isError) {
-      toast.error(message)
-    }
+	useEffect(() => {
+		if (isError) {
+			toast.error(message)
+		}
 
-    if (isSuccess || user) {
-      navigate('/')
-    }
+		if (isSuccess || user) {
+			navigate('/')
+		}
 
-    dispatch(reset())
-
-  }, [user, isError, isSuccess, message, navigate, dispatch])
+		dispatch(reset())
+	}, [user, isError, isSuccess, message, navigate, dispatch])
 
 	const onChange = (e) => {
 		setFormData((prevState) => ({
@@ -44,20 +45,22 @@ function Register() {
 	const onSubmit = (e) => {
 		e.preventDefault()
 
-    if (password != password2) {
-      toast.error('Passwords do not match')
-    } else {
-      const userData = {
-        name, email, password,
-      }
+		if (password != password2) {
+			toast.error('Passwords do not match')
+		} else {
+			const userData = {
+				name,
+				email,
+				password,
+			}
 
-      dispatch(register(userData))
-    }
+			dispatch(register(userData))
+		}
 	}
 
-  if (isLoading) {
-   return <Spinner /> 
-  }
+	if (isLoading) {
+		return <Spinner />
+	}
 
 	return (
 		<>
